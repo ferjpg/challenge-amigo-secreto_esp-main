135341f5755824e3d1bae92d65d4dc5ea3726331
ferjpg <fermr.dev@gmail.com>
2025-03-01 16:17:33 -0600

    Crea funcion asignarTextoId

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregaAmigos(amigo) {
-    
+function asignarTextoId(id,texto) {
+    let idHTML = document.getElementById(id);
+    idHTML.innerHTML = texto;
+    return;
 }
