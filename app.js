41e0f26740ad53b075e737a1cb87101034d15022
ferjpg <fermr.dev@gmail.com>
2025-03-01 16:21:37 -0600

    Crea la funcion agregarAmigo

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ function asignarTextoId(id,texto) {
     idHTML.innerHTML = texto;
     return;
 }
+
+function agregarAmigo() {
+    let escribeAmigo = document.querySelector('input').value;
+    console.log(escribeAmigo);
+    if (escribeAmigo === "") {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        asignarTextoId('listaAmigos', escribeAmigo);
+        return;
+    }  
+}
