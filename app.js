3bb24a4af1121ea7d9aea99b038f129b3a790be8
ferjpg <fermr.dev@gmail.com>
2025-03-02 18:27:30 -0600

    Crea funcion limpiarInput

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+
 function asignarTextoId(id,texto) {
     let idHTML = document.getElementById(id);
     idHTML.innerHTML = texto;
@@ -8,12 +9,20 @@ function asignarTextoId(id,texto) {
 }
 
 function agregarAmigo() {
-    let escribeAmigo = document.querySelector('input').value;
+    let escribeAmigo = document.querySelector('#amigo').value;
     console.log(escribeAmigo);
     if (escribeAmigo === "") {
         alert('Por favor, inserte un nombre.');
     } else {
-        asignarTextoId('listaAmigos', escribeAmigo);
-        return;
-    }  
+        amigos.push(escribeAmigo);
+        actualizaLista();
+        limpiarInput();
+        
+    }
 }
+
+function limpiarInput() {
+    let valorCaja = document.querySelector('#amigo');
+    valorCaja.value = '';
+}
+
