8d2e5686ea517f52632e72fa6632b0de5a1c5df7
ferjpg <fermr.dev@gmail.com>
2025-03-02 21:18:40 -0600

    Crea funcion sortearAmigo

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ function asignarTextoId(id,texto) {
 
 function agregarAmigo() {
     let escribeAmigo = document.querySelector('#amigo').value;
-    console.log(escribeAmigo);
     if (escribeAmigo === "") {
         alert('Por favor, inserte un nombre.');
     } else {
@@ -33,5 +32,15 @@ function actualizaLista() {
         let li = document.createElement("li");
         li.textContent = amigos[i];
         lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No se ha insertado ningun nombre.');
+    } else {
+        asignarTextoId('listaAmigos', '');
+        let numero = Math.floor(Math.random() * amigos.length);
+        asignarTextoId('resultado', `El amigo secreto es: ${amigos[numero]}`);
     }
 }
