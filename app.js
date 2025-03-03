9fe77c281f31bb98471d3834294f30c903e92d22
ferjpg <fermr.dev@gmail.com>
2025-03-02 18:28:40 -0600

    Crea funcion actualizaLista

--- app.js
+++ app.js
@@ -26,3 +26,12 @@ function limpiarInput() {
     valorCaja.value = '';
 }
 
+function actualizaLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; 
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
